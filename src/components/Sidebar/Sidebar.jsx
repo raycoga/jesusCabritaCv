70f85e417f6ae68de372382eb83a299d5fa2585e
raycoga <capram94@gmail.com>
2022-10-08 22:50:05 -0400

    avatar style

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,15 +1,20 @@
 import React, { memo, useState } from "react";
 import { Layout } from "antd";
 import "./Sidebar.scss";
+import Avatar from "../Avatar/Avatar";
 const { Sider } = Layout;
 const Sidebar = memo(() => {
   const [collapsed, setCollapsed] = useState(false);
   return (
     <Sider
+      className="custom-sider"
       collapsible
       collapsed={collapsed}
       onCollapse={(value) => setCollapsed(value)}
-    ></Sider>
+    >
+      <Avatar collapsed={collapsed} />
+      12
+    </Sider>
   );
 });
 Sidebar.displayName = "Sidebar";
